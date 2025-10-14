a0365829bd48406f5ae849e7fd6a7b92f9e29867
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-10-14 22:18:30 +0900

    style: 불필요한 파일 및 주석 정리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,9 +4,6 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        // 덧셈할 문자열을 입력해 주세요.
-        // 1,2:3
-        // 결과 : 6
         var calculator = new Calculator();
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         var line = Console.readLine();
--- src/main/java/calculator/Constant.java
+++
@@ -1,9 +0,0 @@
-package calculator;
-
-public final class Constant {
-    private Constant() {
-    }
-
-    // 구분자
-    public static final char[] 기본_구분자_배열 = new char[]{',', ':'};
-}
