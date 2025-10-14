c8b1f105dd3b4e335af246310ffb567f1c6786b0
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-10-14 21:53:21 +0900

    feat: 입/출력 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,10 +1,16 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        try {
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
-        }
+        // 덧셈할 문자열을 입력해 주세요.
+        // 1,2:3
+        // 결과 : 6
+        var calculator = new Calculator();
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        var line = Console.readLine();
+        var res = calculator.add(line);
+        System.out.println(String.format("결과 : %d", res));
     }
 }
