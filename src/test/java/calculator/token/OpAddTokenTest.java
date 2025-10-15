0082131fe188b0560030911957bb0e83d4ab0e16
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-10-15 21:24:17 +0900

    test(OpAddToken): 가능한 연산 경우의 수 테스트 추가

--- src/test/java/calculator/token/OpAddTokenTest.java
+++ src/test/java/calculator/token/OpAddTokenTest.java
@@ -1,25 +1,49 @@
-package calculator;
+package calculator.token;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class OpAddTokenTest {
 
-    @Test
-    void testReduce_1() {
-        var token1 = new OpAddToken(1);
-        var token2 = new IntegerToken(2);
-        var token3 = new IntegerToken(3);
+    private OpAddToken adder;
 
-        assertEquals(token3, token1.reduce(token2));
+    @BeforeEach
+    void setUp() {
+        adder = new OpAddToken();
+    }
+
+    // 1. adder integerToken == integerToken
+    // 2. adder adder == integerToken
+    // 3. adder adder == adder
+
+    @Test
+    void test_adderReduceIntegerToken_equalsIntegerToken() {
+        var seven = 7;
+        var sevenToken1 = new IntegerToken(seven);
+        var sevenToken2 = new IntegerToken(seven);
+
+        // 7 = 0+ 7
+        assertEquals(sevenToken2, adder.reduce(sevenToken1));
     }
 
     @Test
-    void testReduce_2() {
-        var token1 = new OpAddToken(1);
-        var token2 = new IntegerToken(2);
-        var token3 = new OpAddToken(3);
+    void test_adderReduceAdder_equalsIntegerToken() {
+        var seven = 7;
+        var sevenToken1 = new OpAddToken(seven);
+        var sevenToken2 = new IntegerToken(seven);
 
-        assertEquals(token3, token1.reduce(token2));
+        // 7 = 0+ 7
+        assertEquals(sevenToken2, adder.reduce(sevenToken1));
+    }
+
+    @Test
+    void test_adderReduceAdder_equalsAdder() {
+        var seven = 7;
+        var sevenToken1 = new OpAddToken(seven);
+        var sevenToken2 = new OpAddToken(seven);
+
+        // 7 = 0+ 7
+        assertEquals(sevenToken2, adder.reduce(sevenToken1));
     }
 }
