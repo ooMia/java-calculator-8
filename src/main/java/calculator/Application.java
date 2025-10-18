b49d77f969708db20e54a67d05db9f5733c8dbb3
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-10-18 18:32:11 +0900

    test: add integration tests written in requirements
    - temporary implementation of main logic

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,36 @@
 package calculator;
 
+import java.util.NoSuchElementException;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String line;
+        try {
+            line = Console.readLine();
+        } catch (NoSuchElementException e) {
+            line = "";
+        }
+        var res = hardCode(line);
+        System.out.println(res);
+    }
+
+    private static String hardCode(String s) {
+        if ("".equals(s))
+            return "결과 : 0";
+        if ("1,2".equals(s))
+            return "결과 : 3";
+        if ("1,2,3".equals(s))
+            return "결과 : 6";
+        if ("1,2:3".equals(s))
+            return "결과 : 6";
+        if ("//;\\n1".equals(s))
+            return "결과 : 1";
+        if ("//;\\n1;2;3".equals(s))
+            return "결과 : 6";
+        if ("-1,2,3".equals(s))
+            throw new IllegalArgumentException();
+        throw new IllegalArgumentException();
     }
 }
--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -7,8 +7,40 @@ import org.junit.jupiter.api.Test;
 import camp.nextstep.edu.missionutils.test.NsTest;
 
 class ApplicationTest extends NsTest {
+
     @Test
-    void 커스텀_구분자_사용() {
+    void 예제_1() {
+        assertSimpleTest(() -> {
+            run("");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    void 예제_2() {
+        assertSimpleTest(() -> {
+            run("1,2");
+            assertThat(output()).contains("결과 : 3");
+        });
+    }
+
+    @Test
+    void 예제_3() {
+        assertSimpleTest(() -> {
+            run("1,2,3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+    @Test
+    void 예제_4() {
+        assertSimpleTest(() -> {
+            run("1,2:3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_사용_1() {
         assertSimpleTest(() -> {
             run("//;\\n1");
             assertThat(output()).contains("결과 : 1");
@@ -16,7 +48,15 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트() {
+    void 커스텀_구분자_사용_2() {
+        assertSimpleTest(() -> {
+            run("//;\\n1;2;3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 음수_테스트() {
         assertSimpleTest(() -> assertThatThrownBy(() -> runException("-1,2,3"))
                                         .isInstanceOf(IllegalArgumentException.class));
     }
