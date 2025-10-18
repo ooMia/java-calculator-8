b6684b162fdd10425f861773a9cdac1e7eca6dc7
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-10-18 22:47:52 +0900

    feat: finished

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,36 +1,37 @@
 package calculator;
 
 import java.util.NoSuchElementException;
+import calculator.token.AddIntegerOperationToken;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String line;
-        try {
-            line = Console.readLine();
-        } catch (NoSuchElementException e) {
-            line = "";
-        }
-        var res = hardCode(line);
-        System.out.println(res);
+        String line = readLine();
+        var res = process(line);
+        System.out.printf("결과 : %d", res);
     }
 
-    private static String hardCode(String s) {
-        if ("".equals(s))
-            return "결과 : 0";
-        if ("1,2".equals(s))
-            return "결과 : 3";
-        if ("1,2,3".equals(s))
-            return "결과 : 6";
-        if ("1,2:3".equals(s))
-            return "결과 : 6";
-        if ("//;\\n1".equals(s))
-            return "결과 : 1";
-        if ("//;\\n1;2;3".equals(s))
-            return "결과 : 6";
-        if ("-1,2,3".equals(s))
+    private static String readLine() {
+        try {
+            return Console.readLine();
+        } catch (NoSuchElementException e) {
+            return "";
+        }
+    }
+
+    private static int process(String line) {
+        if (line.isEmpty()) {
+            return 0;
+        }
+        var operator = new AddIntegerOperationToken();
+        try {
+            for (char c : line.toCharArray()) {
+                operator.reduce(c);
+            }
+            return operator.getFinalResult().intValue();
+        } catch (Exception e) {
             throw new IllegalArgumentException();
-        throw new IllegalArgumentException();
+        }
     }
 }
